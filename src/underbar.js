80ff8b8804829d028ea6a9b6c002ea4415c146fa
Darion Freeman <darionfman@gmail.com>
2016-08-14 12:00:07 -0500

    Delay passes

--- src/underbar.js
+++ src/underbar.js
@@ -322,6 +322,10 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var argDelay = Array.prototype.slice.call(arguments, 2);
+    setTimeout(function(){
+      func.apply(this, argDelay);
+    }, wait);
   };
 
 
